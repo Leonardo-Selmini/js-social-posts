f96ac61bee6901b5d0a61af583d3b5e1aa75201c
Leonardo.Selmini <selminileonardo@gmail.com>
2021-11-22 16:34:34 +0100

    create personalized cards

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ const posts = [
     {
         "id": 1,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/300?image=171",
+        "media": "https://unsplash.it/600/400?image=118",
         "author": {
             "name": "Phil Mangione",
             "image": "https://unsplash.it/300/300?image=15"
         },
         "likes": 80,
-        "created": "2021-06-25"
+        'created': "2021-06-25"
     },
     {
         "id": 2,
@@ -38,7 +38,7 @@ const posts = [
         "media": "https://unsplash.it/600/400?image=24",
         "author": {
             "name": "Luca Formicola",
-            "image": null
+            "image": "https://unsplash.it/300/300?image=72"
         },
         "likes": 56,
         "created": "2021-04-03"
@@ -55,3 +55,39 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+for (let i = 0; i < posts.length; i++) {
+    let container = document.getElementById('container');
+    container.innerHTML += `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src=${posts[i].author.image} alt=${posts[i].author.name}>
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${posts[i].author.name}</div>
+                        <div class="post-meta__time">${posts[i].created}</div>
+                    </div>
+                </div>
+            </div>
+            <div class="post__text">${posts[i].content}</div>
+                <div class="post__image">
+                    <img src="${posts[i].media}" alt="${posts[i].author.name} Photo">
+                </div>
+                <div class="post__footer">
+                    <div class="likes js-likes">
+                        <div class="likes__cta">
+                            <a class="like-button js-like-button" href="#" data-postid="1">
+                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                <span class="like-button__label">Mi Piace</span>
+                            </a>
+                        </div>
+                        <div class="likes__counter">
+                            Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+};
